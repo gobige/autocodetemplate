bf297546b9256d981d47013fef41767d9741810a
yates <569484515@qq.com>
2018-10-17 22:30:23 +0800

    类方法，递归，普通方法调用执行时间测试

--- src/main/java/com/example/autocodetemplate/ohter/practice/base/MethodTransferTimeTest.java
+++ src/main/java/com/example/autocodetemplate/ohter/practice/base/MethodTransferTimeTest.java
@@ -1,40 +1,30 @@
-package com.example.autocodetemplate.ohter.practice;
-
-import org.springframework.beans.BeanUtils;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+package com.example.autocodetemplate.ohter.practice.base;
 
 /**
- * <p>爱车小屋</p>
- * <p>Project: carhouse-xx</p>
- * <p>ModuleID: xx</p>
- * <p>Comments: xx</p>
- * <p>JDK version used JDK1.8</p>
- *
- * @version 1.0
+ * 递归，类方法，普通方法调用执行时间测试demo(无法得出结论，位于前面的方法，因为第一次调用，调用时间要长一些)
  */
-public class testDel {
+public class MethodTransferTimeTest {
     public static void main(String[] args) {
+        MethodTransferTimeTest transferTimeTest = new MethodTransferTimeTest();
+
+        transferTimeTest.testRecursive(5000);
+
+
+        long RecursivestartTime = System.currentTimeMillis();
+        transferTimeTest.testRecursive(5000);
+        long RecursiveendTime = System.currentTimeMillis();
+
+        long startTime = System.currentTimeMillis();
+        transferTimeTest.testSingleMethod();
+        long endTime = System.currentTimeMillis();
+
         long classStartTime = System.currentTimeMillis();
         testStaticSingleMethod();
         long classEndTime = System.currentTimeMillis();
 
-        testDel testDel = new testDel();
-        long startTime = System.currentTimeMillis();
-        testDel.testSingleMethod();
-        long endTime = System.currentTimeMillis();
-
-
-        long RecursivestartTime = System.currentTimeMillis();
-        testDel.testRecursive(5000);
-        long RecursiveendTime = System.currentTimeMillis();
         System.out.println("递归方法执行时间" + (RecursiveendTime - RecursivestartTime));
         System.out.println("类方法执行时间" + (classEndTime - classStartTime));
         System.out.println("普通方法执行时间" + (endTime - startTime));
-
     }
 
     public void testRecursive(int num) {
@@ -60,19 +50,3 @@ public class testDel {
 
     }
 }
-
-class test1 extends test implements testInteface {
-
-}
-
-class test implements testInteface {
-
-}
-
-interface testInteface {
-    static String test() {
-        return "test interface";
-    }
-}
-
-
