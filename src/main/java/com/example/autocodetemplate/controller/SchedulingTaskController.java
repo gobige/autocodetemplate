7bd1503abd9398413028718b45b69a8373daad40
yates <569484515@qq.com>
2018-12-06 23:53:31 +0800

    @DateTimeFormat  jackson无法支持date反序列字符串的解决方案

--- src/main/java/com/example/autocodetemplate/controller/SchedulingTaskController.java
+++ src/main/java/com/example/autocodetemplate/controller/SchedulingTaskController.java
@@ -1,7 +1,10 @@
 package com.example.autocodetemplate.controller;
 
+import com.example.autocodetemplate.util.TimeUtil;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * 定时任务类
@@ -15,8 +18,8 @@ public class SchedulingTaskController {
      * initialDelay 初始化时延迟时间，执行，之后正常执行
      * cron="" 根据cron表达式配置执行计划
      */
-    @Scheduled(cron = "0/30 * * * * ? ")
+    @Scheduled(cron = "0 0 2 1 * ?")
     public void reportFindGirl() {
-        System.out.println("七夕节到了，还不找女朋友，你妈又要催了");
+        System.out.println("七夕节到了，还不找女朋友，你妈又要催了!"+ TimeUtil.dateToLocalDateTime(new Date()));
     }
 }
