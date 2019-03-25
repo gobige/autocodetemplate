2189522081633ca356a77abbd3f55a2771bce0ed
yangchao <yangchao@car-house.net>
2019-03-25 17:27:27 +0800

    配置文件流读取的几种方式

--- src/main/java/com/example/autocodetemplate/ohter/practice/IO/ConfigFileReadTest.java
+++ src/main/java/com/example/autocodetemplate/ohter/practice/IO/ConfigFileReadTest.java
@@ -1,8 +1,14 @@
 package com.example.autocodetemplate.ohter.practice.IO;
 
-import java.io.IOException;
-import java.io.InputStream;
+import org.apache.catalina.core.ApplicationContext;
+
+import javax.servlet.ServletContext;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 /**
  * <p>爱车小屋</p>
@@ -18,23 +24,81 @@ public class ConfigFileReadTest {
         ConfigFileReadTest configFileReadTest = new ConfigFileReadTest();
 
         InputStream inputStream = configFileReadTest.getByClassLoader("/test.properties");
-        Properties prop = new Properties();
-        prop.load(inputStream);
-        String key = prop.getProperty("test.name");
-         System.out.println(key);
+        InputStream fileInputStream = configFileReadTest.getByFile("C:\\Users\\爱车小屋\\Documents\\GitHub\\autocodetemplate\\src\\main\\resources\\test.properties");
+
+        Properties prop = configFileReadTest.getPropertiesFromInputStream(fileInputStream);
+        String propkey = prop.getProperty("test.name");
+
+        // 流只能被导入加载一次 和上面处理方式类似  只是 把properties 封装成hashmap结构
+        ResourceBundle resource = new PropertyResourceBundle(fileInputStream);
+        String resourcekey = resource.getString("test.name");
+
+        ResourceBundle resourceBundle = configFileReadTest.getResourceBundle("test");
+        String Bundlekey = resourceBundle.getString("test.name");
+
+//        InputStream urlInputStream = configFileReadTest.getByUrl("/test.properties");
+
+        System.out.println(Bundlekey);
     }
 
+//    InputStream getByUrl(String filePath) throws IOException {
+//        ServletContext servletContext = new ApplicationContext();
+//        InputStream in = context.getResourceAsStream(filePath);
+//        return in;
+//    }
+
+    InputStream getByUrl(String filePath) throws IOException {
+        URL url = new URL(filePath);
+
+        return url.openStream();
+    }
+
+    /**
+     * 通过java提供的文件流方式
+     * @param filePath
+     * @return
+     * @throws FileNotFoundException
+     */
+    InputStream getByFile(String filePath) throws FileNotFoundException {
+        InputStream inStream = new FileInputStream(new File(filePath));
+
+        return inStream;
+    }
+
+    /**
+     * 通过类加载器读取配置文件
+     * @param path
+     * @return
+     */
     InputStream getByClassLoader(String path) {
-        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(path);
         InputStream inputStream2 = this.getClass().getResourceAsStream(path);
 
-        return inputStream;
+        // TODO 获取流为null，但是classLoader又不为null（appclassloader），获取URL为null
+        InputStream inputStream3 = ClassLoader.getSystemResourceAsStream(path);
+
+        // TODO 获取流为null，但是classLoader又不为null， 与上面方法一样都是通过类加载器，获取文件路由
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(path);
+
+        return inputStream2;
     }
 
+    /**
+     * 通过properties加载流文件
+     * @param inputStream
+     * @return
+     * @throws IOException
+     */
     Properties getPropertiesFromInputStream(InputStream inputStream) throws IOException {
         Properties prop = new Properties();
         prop.load(inputStream);
 
         return prop;
+    }
+
+    ResourceBundle getResourceBundle(String path) {
+        ResourceBundle resource = ResourceBundle.getBundle(path);
+
+        return resource;
     }
 }
