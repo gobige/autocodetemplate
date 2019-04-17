d1610668f8c4e3a235f41fa0c3d52e4475acb9c6
yangchao <yangchao@car-house.net>
2019-04-17 18:32:32 +0800

    leetcode

--- src/main/java/com/example/autocodetemplate/ohter/practice/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/example/autocodetemplate/ohter/practice/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -7,31 +7,95 @@ import java.util.Map;
  * <p>爱车小屋</p>
  * <p>Project: carhouse-xx</p>
  * <p>ModuleID: xx</p>
- * <p>Comments: xx</p>
+ * <p>Comments: 获取无重复字符的最长子串</p>
  * <p>JDK version used JDK1.8</p>
  *
  * @version 1.0
  */
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
-        String str = "dvdf";
-        int len = lengthOfLongestSubstring(str);
+        String str = "angwdvbdfanplw";
 
-        System.out.println(len);
+        int maxSingle = lengthOfLongestSubstring(str);
 
+        System.out.println(maxSingle);
     }
 
+    /**
+     *  获取无重复字符的最长子串
+     *      * 时间复杂度 最坏O(1/2 * n平方) 最好O(N)
+     *      优化：针对重复字符点进行多位滑动
+     * @param s
+     * @return
+     */
     public static int lengthOfLongestSubstring(String s) {
-        if (s == null) {
-            return 0;
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
         }
-        char[] chars = s.toCharArray();
-        int[] charCorrespondNums = getCharCorrespondNums(chars);
-        int count =  getCharCorrespondNums(charCorrespondNums);
-
-        return count;
+        return ans;
     }
 
+    /**
+     *  获取无重复字符的最长子串
+     *      * 时间复杂度 最坏O(1/2 * n平方) 最好O(N)
+     * @param s
+     * @return
+     */
+//    public static int lengthOfLongestSubstring(String s) {
+//        char[] chars = s.toCharArray();
+//
+//        Map<Character, Character> characterMap = new HashMap<>(chars.length);
+//        Integer maxSingle = 0;
+//        Integer index = 0;
+//        for (int i = 0; i < chars.length; ) {
+//            if (characterMap.containsKey(chars[i])) {
+//                if (maxSingle < characterMap.size()) {
+//                    maxSingle = characterMap.size();
+//                }
+//                index++;
+//                i = index;
+//                characterMap.clear();
+//            } else {
+//                characterMap.put(chars[i], chars[i]);
+//                i++;
+//            }
+//        }
+//
+//        if (maxSingle < characterMap.size()) {
+//            maxSingle = characterMap.size();
+//        }
+//
+//        return maxSingle;
+//    }
+
+    /**
+     * 失败算法
+     * @param s
+     * @return
+     */
+//    public static int lengthOfLongestSubstring(String s) {
+//        if (s == null) {
+//            return 0;
+//        }
+//        char[] chars = s.toCharArray();
+//        int[] charCorrespondNums = getCharCorrespondNums(chars);
+//        int count =  getCharCorrespondNums(charCorrespondNums);
+//
+//        return count;
+//    }
+
+    /**
+     * 排列所有字符第几次出现，为1的肯定是不重复的，但是会出现好前缀遗漏的现象 失败算法
+     * @param charCorrespondNums
+     * @return
+     */
     public static int getCharCorrespondNums(int[] charCorrespondNums) {
         int count = 0;
         if (charCorrespondNums != null && charCorrespondNums.length > 0) {
@@ -52,6 +116,11 @@ public class LongestSubstringWithoutRepeatingCharacters {
         return count;
     }
 
+    /**
+     * 获取每个字符在字符串中出现的次数
+     * @param chars
+     * @return
+     */
     public static int[] getCharCorrespondNums(char[] chars) {
         int[] charCorrespondNums = new int[chars.length];
 
