86a62ea8184ae852dd5a84c1760bff52880ec6fb
yates <569484515@qq.com>
2019-04-21 22:26:02 +0800

    回文数

---
+++ src/main/java/com/example/autocodetemplate/ohter/practice/leetcode/NumberOfTracts.java
@@ -0,0 +1,33 @@
+package com.example.autocodetemplate.ohter.practice.leetcode;
+
+public class NumberOfTracts {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(10));
+    }
+
+
+    public static boolean isPalindrome(int x) {
+        String xstr = String.valueOf(x);
+
+        char[] xchars = xstr.toCharArray();
+        char[] reverseXChars = new char[xchars.length];
+
+        int j = xchars.length - 1;
+        for (int i = 0; i < xchars.length; i++, j--) {
+            reverseXChars[i] = xchars[j];
+        }
+
+        try {
+            int reverseX = Integer.valueOf(new String(reverseXChars));
+
+            if (reverseX == x) {
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+
+        return false;
+    }
+}
