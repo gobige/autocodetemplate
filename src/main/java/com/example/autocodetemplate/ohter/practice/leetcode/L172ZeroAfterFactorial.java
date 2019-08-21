564c9eb69bb546204f25cada8c1789b2c174403f
yangchao <yangchao@car-house.net>
2019-08-21 18:24:46 +0800

    leetcode practice

---
+++ src/main/java/com/example/autocodetemplate/ohter/practice/leetcode/L172ZeroAfterFactorial.java
@@ -0,0 +1,10 @@
+package com.example.autocodetemplate.ohter.practice.leetcode;
+
+/**
+ *  阶乘后的零
+ *
+ * 给定一个整数 n，返回 n! 结果尾数中零的数量。
+ *
+ */
+public class L172ZeroAfterFactorial {
+}
---
+++ src/main/java/com/example/autocodetemplate/ohter/practice/leetcode/L175CombineTwoTables.java
@@ -0,0 +1,12 @@
+package com.example.autocodetemplate.ohter.practice.leetcode;
+
+/**
+ * 175. 组合两个表
+ */
+public class L175CombineTwoTables {
+    public static void main(String[] args) {
+        System.out.println("");
+
+
+    }
+}
