ac8f73d22e772bdf5904033351af59dece34e434
yangchao <yangchao@car-house.net>
2019-10-12 19:22:41 +0800

    leetcode practice

---
+++ src/main/java/com/example/autocodetemplate/ohter/practice/leetcode/L606CreatesAStringBasedOnABinaryTree.java
@@ -0,0 +1,25 @@
+package com.example.autocodetemplate.ohter.practice.leetcode;
+
+/**
+ * 606. 根据二叉树创建字符串
+ * 你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+ *
+ * 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+ *
+ */
+public class L606CreatesAStringBasedOnABinaryTree {
+    public String tree2str(TreeNode t) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (t != null) {
+
+        }
+    }
+
+
+    public static void preorderTraversal(TreeNode treeNode, StringBuilder stringBuilder) {
+        if (treeNode != null) {
+            stringBuilder
+        }
+    }
+}
