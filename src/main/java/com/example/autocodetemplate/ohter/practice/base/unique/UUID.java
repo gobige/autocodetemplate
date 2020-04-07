73799b808f122c2ab5ba7e5d13f5beedc45ba196
yangchao <yangchao@car-house.net>
2020-04-07 17:55:13 +0800

    practices

---
+++ src/main/java/com/example/autocodetemplate/ohter/practice/base/unique/UUID.java
@@ -0,0 +1,7 @@
+package com.example.autocodetemplate.ohter.practice.base.unique;
+
+public class UUID {
+    public static void main(String[] args) {
+        System.out.println(java.util.UUID.randomUUID().toString());   ;
+    }
+}
