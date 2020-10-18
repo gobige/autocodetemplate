9d09915af34cde211865263322d8269f151bf2c5
yates <gobige@users.noreply.github.com>
2020-10-18 16:45:45 +0800

    常见 正则匹配 练习

---
+++ src/test/java/com/example/autocodetemplate/service/RocketMQTest.java
@@ -0,0 +1,196 @@
+package com.example.autocodetemplate.service;
+
+import com.example.autocodetemplate.domain.UserCreateMessage;
+import com.example.autocodetemplate.rabbit.Exchange;
+import com.example.autocodetemplate.rabbit.RoutingKey;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.MessageSelector;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.*;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class RocketMQTest {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testSendMq() throws Exception {
+        // 实例化消息生产者Producer
+        DefaultMQProducer producer = new DefaultMQProducer("producerGroup1");
+        // 设置NameServer的地址
+        producer.setNamesrvAddr("localhost:9876");
+        // 启动Producer实例
+        producer.start();
+
+        // 2 设置异步发送
+//        producer.setRetryTimesWhenSendAsyncFailed(0);
+
+        for (int i = 0; i < 100; i++) {
+            // 创建消息，并指定Topic，Tag和消息体
+            Message msg = new Message("TopicTest", "TagA",
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+            // 发送消息到一个Broker
+//          SendResult sendResult = producer.send(msg);
+
+            // 批量发送消息 1 相同topic   2不能是延时 3总大小不能超过4M
+//          SendResult sendResult = producer.send(Arrays.asList(msg));
+
+            // 5 设置延时等级3,这个消息将在10s之后发送(现在只支持固定的几个时间,详看delayTimeLevel)
+//            msg.setDelayTimeLevel(3);
+
+            // 4按照规则进行消息负载且顺序分发
+            SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                @Override
+                public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                    // 根据订单id选择发送queue
+                    Long id = (Long) arg;
+                    long index = id % mqs.size();
+                    return mqs.get((int) index);
+                }
+            }, i);
+
+
+            // 3 发送单向消息，没任何返回结果
+//            producer.sendOneway(msg);
+
+            // 通过sendResult返回消息是否成功送达
+            System.out.printf("%s%n", sendResult);
+        }
+
+        // 如果不再发送消息，关闭Producer实例。
+        producer.shutdown();
+
+    }
+
+    public class TransactionListenerImpl implements TransactionListener {
+        private AtomicInteger transactionIndex = new AtomicInteger(0);
+        private ConcurrentHashMap<String, Integer> localTrans = new ConcurrentHashMap<>();
+        @Override
+        public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
+            int value = transactionIndex.getAndIncrement();
+            int status = value % 3;
+            localTrans.put(msg.getTransactionId(), status);
+            return LocalTransactionState.UNKNOW;
+        }
+        @Override
+        public LocalTransactionState checkLocalTransaction(MessageExt msg) {
+            Integer status = localTrans.get(msg.getTransactionId());
+            if (null != status) {
+                switch (status) {
+                    case 0:
+                        return LocalTransactionState.UNKNOW;
+                    case 1:
+                        return LocalTransactionState.COMMIT_MESSAGE;
+                    case 2:
+                        return LocalTransactionState.ROLLBACK_MESSAGE;
+                }
+            }
+            return LocalTransactionState.COMMIT_MESSAGE;
+        }
+    }
+
+    /**
+     * 生产者事务实现
+     * @throws Exception
+     */
+    @Test
+    public void transactionProducer()  throws Exception{
+
+        TransactionListener transactionListener = new TransactionListenerImpl();
+        TransactionMQProducer producer = new TransactionMQProducer("please_rename_unique_group_name");
+        ExecutorService executorService = new ThreadPoolExecutor(2, 5, 100, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(2000), new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                thread.setName("client-transaction-msg-check-thread");
+                return thread;
+            }
+        });
+        producer.setExecutorService(executorService);
+        producer.setTransactionListener(transactionListener);
+        producer.start();
+        String[] tags = new String[] {"TagA", "TagB", "TagC", "TagD", "TagE"};
+        for (int i = 0; i < 10; i++) {
+            try {
+                Message msg =
+                        new Message("TopicTest1234", tags[i % tags.length], "KEY" + i,
+                                ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                SendResult sendResult = producer.sendMessageInTransaction(msg, null);
+                System.out.printf("%s%n", sendResult);
+                Thread.sleep(10);
+            } catch (MQClientException | UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        for (int i = 0; i < 100000; i++) {
+            Thread.sleep(1000);
+        }
+        producer.shutdown();
+    }
+
+    @Test
+    public void testReceiveMq() throws Exception {
+
+        // 实例化消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumerGroup1");
+
+        // 设置NameServer的地址
+        consumer.setNamesrvAddr("localhost:9876");
+
+        /**
+         * 设置Consumer第一次启动是从队列头部开始消费还是队列尾部开始消费<br>
+         * 如果非第一次启动，那么按照上次消费的位置继续消费
+         */
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+        // 订阅一个或者多个Topic，以及Tag来过滤需要消费的消息
+//        consumer.subscribe("TopicTest", "TagA || TagC || TagD");
+        consumer.subscribe("TopicTest", "*");
+
+
+        // 过滤消息
+//        consumer.subscribe("TopicTest", MessageSelector.bySql("a between 0 and 3");
+
+        // 注册回调实现类来处理从broker拉取回来的消息
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+        // 顺序消费
+//      consumer.registerMessageListener(new MessageListenerOrderly() {
+
+                @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                // 标记该消息已经被成功消费
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        // 启动消费者实例
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
