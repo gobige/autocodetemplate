d12a184a05961b499c722c00670bb5fb1a26c92c
yates <gobige@users.noreply.github.com>
2022-07-13 14:45:36 +0800

    anotation test

--- src/main/java/com/example/autocodetemplate/ohter/practice/base/AnnotationTest.java
+++ src/main/java/com/example/autocodetemplate/ohter/practice/base/AnnotationTest.java
@@ -1,10 +1,20 @@
 package com.example.autocodetemplate.ohter.practice.base;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatVisitorWrapper;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+import com.fasterxml.jackson.databind.ser.std.StdScalarSerializer;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.lang.annotation.*;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Objects;
 
 @MyIF(author = "muyibeyond", desc = "test anotation class")
 public class AnnotationTest {
@@ -47,4 +57,96 @@ public class AnnotationTest {
     String author() default "yates";
 
     String desc() default "";
+}
+
+//////////////////////////////////////////////////
+
+@Target({ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+  @interface DataMasking {
+
+    DataMaskingFunc maskFunc() default DataMaskingFunc.NO_MASK;
+
+}
+
+  interface DataMaskingOperation {
+
+    String MASK_CHAR = "*";
+
+    String mask(String content, String maskChar);
+
+}
+
+  enum DataMaskingFunc {
+
+    /**
+     *  脱敏转换器
+     */
+    NO_MASK((str, maskChar) -> {
+        return str;
+    }),
+    ALL_MASK((str, maskChar) -> {
+        if (StringUtils.hasLength(str)) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < str.length(); i++) {
+                sb.append(StringUtils.hasLength(maskChar) ? maskChar : DataMaskingOperation.MASK_CHAR);
+            }
+            return sb.toString();
+        } else {
+            return str;
+        }
+    });
+
+    private final DataMaskingOperation operation;
+
+    private DataMaskingFunc(DataMaskingOperation operation) {
+        this.operation = operation;
+    }
+
+    public DataMaskingOperation operation() {
+        return this.operation;
+    }
+
+}
+
+  final class DataMaskingSerializer extends StdScalarSerializer<Object> {
+    private final DataMaskingOperation operation;
+
+    public DataMaskingSerializer() {
+        super(String.class, false);
+        this.operation = null;
+    }
+
+    public DataMaskingSerializer(DataMaskingOperation operation) {
+        super(String.class, false);
+        this.operation = operation;
+    }
+
+
+    public boolean isEmpty(SerializerProvider prov, Object value) {
+        String str = (String)value;
+        return str.isEmpty();
+    }
+
+    public void serialize(Object value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        if (Objects.isNull(operation)) {
+            String content = DataMaskingFunc.ALL_MASK.operation().mask((String) value, null);
+            gen.writeString(content);
+        } else {
+            String content = operation.mask((String) value, null);
+            gen.writeString(content);
+        }
+    }
+
+    public final void serializeWithType(Object value, JsonGenerator gen, SerializerProvider provider, TypeSerializer typeSer) throws IOException {
+        this.serialize(value, gen, provider);
+    }
+
+    public JsonNode getSchema(SerializerProvider provider, Type typeHint) {
+        return this.createSchemaNode("string", true);
+    }
+
+    public void acceptJsonFormatVisitor(JsonFormatVisitorWrapper visitor, JavaType typeHint) throws JsonMappingException {
+        this.visitStringFormat(visitor, typeHint);
+    }
 }
